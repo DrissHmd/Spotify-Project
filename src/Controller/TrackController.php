c1cfa7f6d628a9809356be0004261ae010e1664a
HMIMID DRISS p2106555 <driss.hmimid@etu.univ-lyon1.fr>
2024-10-22 12:06:01 +0200

    tout fini : - Musique (recherche, favoris, affichage infos) - Artist (recherche, favoris, affichage infos) - Connexion/Inscription - Favoris par User

--- src/Controller/TrackController.php
+++ src/Controller/TrackController.php
@@ -2,50 +2,29 @@
 
 namespace App\Controller;
 
-//use App\Factory\TrackFactory;
-use App\Entity\Track;
+use App\Entity\FavoriteTrack;
+use App\Repository\FavoriteTrackRepository;
 use App\Service\AuthSpotifyService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
+#[Route('/track')]
 class TrackController extends AbstractController
 {
     private string $token;
 
     public function __construct(private readonly AuthSpotifyService  $authSpotifyService,
                                 private readonly HttpClientInterface $httpClient,
-//                                private readonly TrackFactory         $trackFactory
     )
     {
         $this->token = $this->authSpotifyService->auth();
     }
 
-    #[Route('/track/{id}', name: 'track_id')]
-    public function getTrack($id): Response
-    {
-        $response = $this->httpClient->request('GET', 'https://api.spotify.com/v1/tracks/'.$id, [
-            'headers' => [
-                'Authorization' => 'Bearer ' . $this->token,
-            ]
-        ]);
-
-        $track_info = $response->toArray();
-        $track = new Track();
-        $track->setName($track_info['name'])
-            ->setPictureLink($track_info['album']['images'][0]['url'])
-            ->setType($track_info['album']['album_type'])
-            ->setId($track_info['id'])
-            ->setArtist($track_info['album']['artists'][0]['name']);
-
-        return $this->render('track/index.html.twig', [
-            'track' => $track,
-        ]);
-    }
-
-    #[Route('/search', name: 'app_search')]
+    #[Route('/search', name: 'app_search_track')]
     public function search(Request $request): Response
     {
         $query = $request->query->get('query');
@@ -67,28 +46,127 @@ class TrackController extends AbstractController
             $items = $response->toArray()['tracks']['items'];
 
             foreach ($items as $item) {
-                $track = new Track();
-                $track->setName($item['name'])
-                    ->setPictureLink($item['album']['images'][0]['url'])
-                    ->setType($item['type'])
-                    ->setId($item['id'])
-                    ->setArtist($item['artists'][0]['name'])
-                    ->setPreviewUrl($item['preview_url'] ?? null);
+                $track = new FavoriteTrack();
+                $track->setTrackName($item['name']);
+                $track->setPicture($item['album']['images'][0]['url']);
+                $track->setType($item['album']['album_type']);
+                $track->setTrackId($item['id']);
+                $track->setArtistName($item['album']['artists'][0]['name']);
 
                 $results[] = $track;
             }
         }
 
-        return $this->render('search/results.html.twig', [
+        return $this->render('search/resultstrack.html.twig', [
             'results' => $results,
             'query' => $query
         ]);
     }
 
     #[Route('/favorite', name: 'app_favorite')]
-    public function favorite(Request $request): ?Response
+    public function favorite(): Response
     {
-        return null;
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->redirectToRoute('app_user_login');
+        }
+
+        $favorites = $user->getTrackFavorites();
+
+        return $this->render('track/favorite.html.twig', [
+            'favorites' => $favorites,
+        ]);
     }
+
+    #[Route('/favorite/add/{id}', name: 'app_favorite_add')]
+    public function addFavorite(string $id, FavoriteTrackRepository $favoriteRepository, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        if (!$user){
+            return $this->redirectToRoute('app_user_login');
+        }
+
+        $existingFavorite = $favoriteRepository->findOneBy(['trackId' => $id]);
+
+        if ($existingFavorite) {
+            return $this->redirectToRoute('app_favorite');
+        }
+
+        $response = $this->httpClient->request('GET', 'https://api.spotify.com/v1/tracks/'.$id, [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $this->token,
+            ]
+        ]);
+
+        $track_info = $response->toArray();
+
+        if ($track_info !== []) {
+            $track = new FavoriteTrack();
+            $track->setTrackName($track_info['name'])
+                ->setTrackId($track_info['id'])
+                ->setArtistName($track_info['artists'][0]['name'])
+                ->setPicture($track_info['album']['images'][0]['url'])
+                ->setType($track_info['album']['album_type']);
+
+            $entityManager->persist($track);
+            $user->addTrackFavorite($track);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Piste ajoutée aux favoris.');
+        } else {
+            $this->addFlash('error', 'Piste introuvable.');
+        }
+
+        return $this->redirectToRoute('app_favorite');
+    }
+
+    #[Route('/favorite/remove/{id}', name: 'app_favorite_remove')]
+    public function removeFavorite(string $id, FavoriteTrackRepository $favoriteRepository, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user){
+            return $this->redirectToRoute('app_user_login');
+        }
+
+        $track = $favoriteRepository->find($id);
+
+        if ($track) {
+            $user->removeTrackFavorite($track);
+            $entityManager->remove($track);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Piste supprimée des favoris.');
+        } else {
+            $this->addFlash('error', 'Piste introuvable.');
+        }
+
+        return $this->redirectToRoute('app_favorite');
+    }
+
+    #[Route('/{id}', name: 'track_id')]
+    public function getTrack($id): Response
+    {
+        $response = $this->httpClient->request('GET', 'https://api.spotify.com/v1/tracks/'.$id, [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $this->token,
+            ]
+        ]);
+
+        $track_info = $response->toArray();
+        $track = new FavoriteTrack();
+        $track->setTrackName($track_info['name']);
+        $track->setPicture($track_info['album']['images'][0]['url']);
+        $track->setType($track_info['album']['album_type']);
+        $track->setTrackId($track_info['id']);
+        $track->setArtistName($track_info['album']['artists'][0]['name']);
+
+        return $this->render('track/index.html.twig', [
+            'track' => $track,
+        ]);
+    }
+
 
 }
